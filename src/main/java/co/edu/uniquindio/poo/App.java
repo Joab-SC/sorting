b3e1f8d6f2d1d021db68ead14e48c88cc010b8c1
Joab-SC <joabl.suarezc@uqvirtual.edu.co>
2024-10-22 08:46:40 -0500

    Se crae un arreglo aleatorio de números

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,11 +1,26 @@
 package co.edu.uniquindio.poo;
-
+import java.util.Random;
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for(int i = 0; i < numbers.length; i ++){
+            numbers[i] = rand.nextInt(100000);
+        }
+
+        System.out.println("Before: ");
+        printArray(numbers);
+    }
+
+    public static void printArray(int[] array){
+        for(int i = 0; i < array.length; i ++){
+            System.out.print(array[i] + " - ");
+        }
+
     }
 }
