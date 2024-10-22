32b212af54c1d46257944d23e40f057a7eb5518b
Joab-SC <joabl.suarezc@uqvirtual.edu.co>
2024-10-22 07:59:10 -0500

    Se crea la clase buble sort con un el constructor y los getters/setters

---
+++ src/main/java/co/edu/uniquindio/poo/bubbleSort.java
@@ -0,0 +1,21 @@
+package co.edu.uniquindio.poo;
+
+public class bubbleSort {
+    private int[] numeros;
+
+    public bubbleSort() {
+        this.numeros = new int[10];
+
+        
+    }
+
+    public int[] getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(int[] numeros) {
+        this.numeros = numeros;
+    }
+
+    
+}
